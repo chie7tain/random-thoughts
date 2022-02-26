dd127a2968d74c73e3afa89f6d67925c274ce557
chei7tain <fredrickokwuobi@gmail.com>
2022-02-26 22:20:39 +0100

    add a service, and injected it into the app-post-list component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import PostModel from '../post.model';
+import { PostsService } from '../posts.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
   @Input() posts: PostModel[] = [];
+  constructor(public postsService: PostsService) {}
+  ngOnInit() {
+    this.posts = this.postsService.getPosts(); 
+  }
 }
